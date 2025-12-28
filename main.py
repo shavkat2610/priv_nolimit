3e0e22410ad948ef1ba204fe0eb9d9d3c49ca49c
shavkat95 <shavkat95@icloud.com>
2025-12-28 23:28:55 +0100

    1234

--- main.py
+++ main.py
@@ -430,7 +430,7 @@ class AppDelegate(NSObject):
         # start_screenshots()
         #start a timer to make screenshots every 5 seconds
         start_time = NSDate.date() #todo: every 2 secs switch between 1. make screenshot 2. use existing screenshot to evaluate
-        self.timer2 = NSTimer.alloc().initWithFireDate_interval_target_selector_userInfo_repeats_(start_time, 1.7, self, 'gameScreenshot:', None, True)
+        self.timer2 = NSTimer.alloc().initWithFireDate_interval_target_selector_userInfo_repeats_(start_time, 2.3, self, 'gameScreenshot:', None, True)
         self.timer2.setTolerance_(0.67)  
         NSRunLoop.currentRunLoop().addTimer_forMode_(self.timer2, NSDefaultRunLoopMode)
         self.timer2.fire()
