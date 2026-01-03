d59cd690c265d0129fdb28f9e752347fe6390a6a
shavkat95 <shavkat95@icloud.com>
2026-01-03 13:48:03 +0100

    1234

--- main.py
+++ main.py
@@ -1182,28 +1182,28 @@ class AppDelegate(NSObject):
                     self.confidence += 0.7                
             if self.equity_flop > 0.6: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 1.7               
+                    self.confidence += 0.7                 
             if self.equity_flop > 0.7: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 2.7                        
+                    self.confidence += 0.7                          
             if self.equity_flop > 0.8: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 3.7         
+                    self.confidence += 0.7          
             if self.equity_flop > 0.9: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 4.7           
+                    self.confidence += 0.7            
             if self.equity_flop > 0.95: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 5.7                                       
+                    self.confidence += 0.7                                      
             if self.equity_flop > 0.96: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 5.7            
+                    self.confidence += 0.7             
             if self.equity_flop > 0.97: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 5.7                    
+                    self.confidence += 0.7                  
             if self.equity_flop > 0.98: # need to adjust confidence, while still learning ...
                 with self.confidence_lock:
-                    self.confidence += 5.7                                                                         
+                    self.confidence += 0.7                                                                        
         decision = self.makeAIDecision_(outputs)
         # with self.mk_comte_carlo_decision_lock:
         #     set_1_1 = self.equity_flop
