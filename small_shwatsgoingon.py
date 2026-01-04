094d344f6b9c559298cc72c3c2a7aa7c821e2d74
shavkat95 <shavkat95@icloud.com>
2026-01-04 10:16:20 +0100

    1234

--- small_shwatsgoingon.py
+++ small_shwatsgoingon.py
@@ -196,16 +196,16 @@ def how_much(im = None):
             else:
                 pixels[i,j] = (255, 255, 255, 255)
     # secs = time.time()
-    data = pytesseract.image_to_data(
-        im2,
-        output_type=Output.DICT,
-        config="--oem 1 --psm 6"
-    )
-    print("\n how_much debug \n")
-    for text, conf in zip(data["text"], data["conf"]):
-        if text.strip():
-            print(text, conf)
-    print("\n how_much debug \n")
+    # data = pytesseract.image_to_data(
+    #     im2,
+    #     output_type=Output.DICT,
+    #     config="--oem 1 --psm 6"
+    # )
+    # print("\n how_much debug \n")
+    # for text, conf in zip(data["text"], data["conf"]):
+    #     if text.strip():
+    #         print(text, conf)
+    # print("\n how_much debug \n")
     raw_data = pytesseract.image_to_string(im2, config="--oem 1 --psm 6 -c tessedit_char_whitelist=B0123456789.")
     print("raw_data how much: "+raw_data)
     data = raw_data.strip()
@@ -633,16 +633,16 @@ def read_own_money(im = None):
                     pixels[i, j] = (10, 10, 10, 255)
                 else:
                     pixels[i,j] = (255, 255, 255, 255)
-    data = pytesseract.image_to_data(
-        im1,
-        output_type=Output.DICT,
-        config="--oem 1 --psm 6"
-    )
-    print("\n read_total_pot_money debug \n")
-    for text, conf in zip(data["text"], data["conf"]):
-        if text.strip():
-            print(text, conf)
-    print("\n read_total_pot_money debug \n")   
+    # data = pytesseract.image_to_data(
+    #     im1,
+    #     output_type=Output.DICT,
+    #     config="--oem 1 --psm 6"
+    # )
+    # print("\n read_total_pot_money debug \n")
+    # for text, conf in zip(data["text"], data["conf"]):
+    #     if text.strip():
+    #         print(text, conf)
+    # print("\n read_total_pot_money debug \n")   
     try:
         raw_data = pytesseract.image_to_string(im1, config="--oem 1 --psm 6 -c tessedit_char_whitelist=0123456789.,B")
     except Exception as e:
