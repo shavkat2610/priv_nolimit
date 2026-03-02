f95a8b54cc94dddc78661877f45a74f0006bbb94
Shavkat <waldapotheker95@gmail.com>
2026-03-02 13:32:01 +0100

    bhjkbuivtzv

--- temp5.py
+++ temp5.py
@@ -4,6 +4,6 @@ from scripts.shavkats_functions import check_if_w8_for_blinds
 
 
 
-im = Image.open("temp_screenshot/test_screenshot_1771160982_1.png")
+im = Image.open("temp_screenshot/check_boxes.png")
 print(check_if_w8_for_blinds(im))
 
