5d3c2e84fb0eab04c72cf508300fbedad69d04bf
shavkat95 <shavkat95@icloud.com>
2026-03-07 20:38:10 +0100

    gydfhgymf

--- temp2.py
+++ temp2.py
@@ -4,7 +4,7 @@ directory = Path("tesseract_training/ground_truth_flies")
 
 for file_path in directory.glob("*.txt"):
     text = file_path.read_text(encoding="utf-8")
-    if text.endswith(" BB") and text[0].isdigit() and "," not in text and len(text)>4:
+    if (text.endswith(" BB") or text.endswith("%")) and text[0].isdigit() and "," not in text and len(text)>4:
         if text[-4].isdigit():
             if text[-5] == ".":
                 for dig_ in text[:-5]:
@@ -19,6 +19,8 @@ for file_path in directory.glob("*.txt"):
                         break
         # text = text.replace("BB", " BB")
     elif text.endswith(" BB") and text[0].isdigit() and len(text)==4:
+        pass
+    elif text.endswith("%") and text[0].isdigit():
         pass
     else:
         print("ALARM: text does not start with Total Pot: "+text + "|TEXT_END| in file: "+str(file_path))
