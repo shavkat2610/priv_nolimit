8dcdcfdc1c035fb5b59e317af4feeb2012ad0b78
Shavkat <waldapotheker95@gmail.com>
2026-03-12 18:54:30 +0100

    agadfg

--- get_position.py
+++ get_position.py
@@ -3,16 +3,16 @@ import time
 from scripts.shavkats_functions import position_the_game
 
 
-time.sleep(1.5)
+time.sleep(2.5)
 
-# position_the_game()
-time.sleep(1.5)
+position_the_game()
+time.sleep(2.5)
 # time.sleep(5)
 print(pyautogui.position())
 im = pyautogui.screenshot(region=(0, 0, 1000, 900))
 pixels = im.load()
 print(pixels[904, 362])
-# im.save("temp_screenshot.png")
+im.save("temp_screenshot.png")
 
 
 
