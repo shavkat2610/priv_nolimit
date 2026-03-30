94f9ebe73c5d6c45a2e5cea7fe5381656c5e893e
Shavkat <waldapotheker95@gmail.com>
2026-03-30 07:57:40 +0200

    bhjk

--- get_position.py
+++ get_position.py
@@ -5,13 +5,15 @@ from scripts.shavkats_functions import position_the_game
 
 time.sleep(2.5)
 
-position_the_game()
+# position_the_game()
 time.sleep(2.5)
 # time.sleep(5)
-print(pyautogui.position())
-im = pyautogui.screenshot(region=(0, 0, 1000, 900))
-pixels = im.load()
-print(pixels[904, 362])
+# print(pyautogui.position())
+position = pyautogui.position()
+print(position)
+im = pyautogui.screenshot(region=(position[0], position[1], 100, 50))
+# pixels = im.load()
+# print(pixels[904, 362])
 im.save("temp_screenshot.png")
 
 
